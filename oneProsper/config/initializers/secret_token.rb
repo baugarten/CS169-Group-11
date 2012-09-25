24aa65f597bce384ecd93aae95ecb199d4629844
Ben Augarten <b.augarten@gmail.com>
2012-09-24 17:42:14 -0700

    Added empty rails app

---
+++ oneProsper/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneProsper::Application.config.secret_token = 'd8c152e0c5584de99e916eba009a1a5479ce928aac6b7e60f22566f52084a5b9d86388bc40afe2b185a511ea4ac96eeebf2180880af87209abae1d22f7fb4b05'
