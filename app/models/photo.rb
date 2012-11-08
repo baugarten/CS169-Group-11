032eb065f851849aed7e9dab73879562605d12b0
Ben Augarten <b.augarten@gmail.com>
2012-11-08 11:31:17 -0800

    Everything passing

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -14,7 +14,7 @@ class Photo < ActiveRecord::Base
       self.input_data = nil
     end
   end
-   def image_file=(input_data)
+  def image_file=(input_data)
     self.filename = input_data.original_filename
     self.content_type = input_data.content_type.chomp
     self.binary_data = input_data.read
