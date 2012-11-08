123de71e2b8373c7f915493af4716dabf7d920c2
Ben Augarten <b.augarten@gmail.com>
2012-11-08 10:49:09 -0800

    Have everything working -- need test coverage

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,20 @@
 class Photo < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
-  # attr_accessible :title, :body
+  attr_accessible :binary_data, :input_data
+  attr_reader :input_data
+  attr_accessor :input_data
+
+  before_validation :init
+
+  def init
+    debugger
+    if self.input_data
+      self.filename = input_data.original_filename
+      self.content_type = input_data.content_type.chomp
+      self.binary_data = input_data.read
+      self.input_data = nil
+    end
+  end
    def image_file=(input_data)
     self.filename = input_data.original_filename
     self.content_type = input_data.content_type.chomp
