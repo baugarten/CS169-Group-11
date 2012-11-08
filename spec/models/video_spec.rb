6694ef53f45cfa32f35836ce39df7f03d3ca7966
Ben Augarten <b.augarten@gmail.com>
2012-11-08 14:24:10 -0800

    Added test

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-descripe Movie do
+describe Video do
   describe 'video_id' do
     it 'should parse a youtube url' do
-      v = mock('Video', :video_id => "http://www.youtube.com/watch?v=JtGchOIRL58&feature=g-vrec")
-      v.id_from_url.should be "JtGchOIRL58"
+      v = Video.new({:video_id => "http://www.youtube.com/watch?v=JtGchOIRL58&feature=g-vrec"})
+      v.id_from_url.should eq "JtGchOIRL58"
     end
 
     it "shouldn't mess up a random string" do
-      v = mock('Video', :video_id => "JtGchOIRL58")
-      v.id_from_url.should be "JtGchOIRL58"
+      v = Video.new({:video_id => "JtGchOIRL58"})
+      v.id_from_url.should eq "JtGchOIRL58"
     end
   end
 end
