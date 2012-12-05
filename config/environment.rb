2a3c26c017993c7116eb43e922433972d0bd4b58
Ben Augarten <b.augarten@gmail.com>
2012-12-04 19:31:31 -0800

    Changed default date format

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+
 # Initialize the rails application
 OneProsper::Application.initialize!
+
+Date::DATE_FORMATS[:default] = "%m/%d/%Y" 
